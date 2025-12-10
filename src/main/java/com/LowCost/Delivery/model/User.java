471cb6cdddd7f809d62913f87c1ba36ac238fcaf
Ratul-hasan9898 <ratulhasan9898@gmail.com>
2025-12-11 00:21:58 +0600

    api connected earnpeople

--- src/main/java/com/LowCost/Delivery/model/User.java
+++ src/main/java/com/LowCost/Delivery/model/User.java
@@ -1,7 +1,16 @@
 package com.LowCost.Delivery.model;
 
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
@@ -10,63 +19,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+  
     private String username;
     private String email;
     private String phone;
     private String password;
+    private String role;
     private String gender;
 
-    public User() {}
 
-    public User(String username, String email, String phone, String password, String gender) {
-        this.username = username;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.gender = gender;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 }
--- src/main/java/com/LowCost/Delivery/repository/UserRepository.java
+++ src/main/java/com/LowCost/Delivery/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.LowCost.Delivery.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.LowCost.Delivery.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    // User findByEmail(String email);
+     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
